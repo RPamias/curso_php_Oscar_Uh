2c02beb95343d65739bc63ffc5e26cab33ace47b
Ramón Pàmias Piqué <ramonpamias@gmail.com>
2024-06-27 11:14:01 +0200

    Ejercicio1 - echo, print_r, print y comentarios

---
+++ EJERCICIOS/Ejercicio1.php
@@ -0,0 +1,18 @@
+
+<?php
+
+// Así se escribe un comentario simple
+
+/*
+Este es un comentario  multilínea.
+Este tipo de comentarios són para varias líneas.
+*/
+
+echo "Hola Develoteca";
+
+// Imprime información sin importar el formato o el estilo. Puedo imprimir cualquier cosa.
+print_r("Hola Develoteca");
+
+// También existe el comando print que nos va a permitir imprimir información también.
+print("Hola Develoteca");
+?>
